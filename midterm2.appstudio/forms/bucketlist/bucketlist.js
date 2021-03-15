caa25494c932e7f99037249ba408b5eec1e01ac3
Matthew Blodgett <matthewblodgett@creighton.edu>
2021-03-15 14:52:22 -0500

    bucketlist

---
+++ midterm2.appstudio/forms/bucketlist/bucketlist.js
@@ -0,0 +1,9 @@
+let bucketlist = []
+let i = 0
+let nomorebucket = false
+while (nomorebucket == false){
+    bucketlist[i] = prompt("Enter the next Bucket list item: ")
+    i++
+    nomorebucket = confirm("Are you done adding to your bucket list?")
+    }
+    console.log(bucketlist)
